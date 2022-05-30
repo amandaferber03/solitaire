502d948a1cf2a29f096e1d8b956993445effbf14
Amanda <aferber2@jhu.edu>
2022-05-30 16:42:11 -0400

    added the Card

---
+++ Card.cpp
@@ -0,0 +1,18 @@
+#ifndef CARD_H
+#define CARD_H
+
+#include <utility>
+#include <string>
+#include <tuple>
+
+namespace Solitaire
+{
+  typedef std::tuple<char, char, char> Position;
+
+  class Card {
+  public:
+
+    virtual ~Card(){}
+
+    bool is_red() const { return red; }
+}
