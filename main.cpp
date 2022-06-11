4e18ab97efeea366f510949a5e642d2bc82d6daf
Amanda <aferber2@jhu.edu>
2022-06-11 19:29:26 -0400

    completed main

--- main.cpp
+++ main.cpp
@@ -2,15 +2,15 @@
 #include <iostream>
 #include <string>
 #include <cassert>
-//#include "Game.h"
+#include "Game.h"
 
 void show_commands() {
-	std::cout << "List of commands:" << std::endl;
-	std::cout << "\t'?':            show this list of options" << std::endl;
-	std::cout << "\t'Q':            quit the game" << std::endl;
-	std::cout << "\t'M' <move>:     try to make the specified move" << std::endl;
-	std::cout << "\t                where <move> is a four character string." << std::endl;
-	std::cout << "\t                The first three characters give the origin position" << std::endl;
+    std::cout << "List of commands:" << std::endl;
+    std::cout << "\t'?':            show this list of options" << std::endl;
+    std::cout << "\t'Q':            quit the game" << std::endl;
+    std::cout << "\t'M' <move>:     try to make the specified move" << std::endl;
+    std::cout << "\t                where <move> is a four character string." << std::endl;
+    std::cout << "\t                The first three characters give the origin position" << std::endl;
     std::cout << "\t                of a card and the last three give its destination position." << std::endl;
     std::cout << "\t                Each two character position identifier indicates a" << std::endl;
     std::cout << "\t                location identifier and a number of cards." << std::endl;
@@ -29,6 +29,47 @@ void show_commands() {
 }
 
 int main(int argc, char* argv[]) {
-    show_commands();
-    return 0; 
+  Solitaire::Game game;
+  show_commands();
+  bool game_end = false;
+
+  while(!game_end) {
+    game.display();
+
+    std::string choice;
+    std::cout << "Next command: ";
+    std::cin >> choice;
+
+    if (choice.length() != 1) {
+      std::cerr << "Action specifier must be a single character, but length(" << choice << ") = " << choice.length() << std::endl;
+    } else {
+      switch (choice[0]) {
+      case '?':
+	show_commands();
+	break;
+      case 'Q': case 'q':
+	game_end = true;
+	break;
+      case 'M': case 'm': {
+	std::string argument;
+	std::cin >> argument;
+	if (argument.length() != 4) {
+	  std::cerr << "Move specifier must be four characters, but length(" << argument << " ) = " << argument.length() << std::endl;
+	} else {
+	  try{
+	    game.make_move(std::make_pair(argument[0], argument[1]), std::make_pair(argument[2], argument[3]));
+	  }
+	  catch (const std::exception& e) {
+	    std::cerr << "Could not make move: " << e.what() << std::endl;
+	  }
+	}
+	break;
+      }
+      default:
+	std::cerr << "Invalid action '" << choice << "'" << std::endl;
+      }
+    }
+  }
+  game.erase_board();  
+  return 0; 
 }
