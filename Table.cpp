34d98130917a7040953f11e2008b61203fde14d2
amandaferber03 <98185007+amandaferber03@users.noreply.github.com>
2022-06-18 23:28:34 -0400

    removed terminal for testing

--- Table.cpp
+++ Table.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <utility>
 #include <map>
+//#include "Terminal.h"
 #include <vector>
-#include "Terminal.h"
 #include "Table.h"
 #include "CreatePiece.h"
 #include "Exceptions.h"
