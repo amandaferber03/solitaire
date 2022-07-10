2c1f7ee31b511b111dc3533988069b01b42fcfae
amandaferber03 <98185007+amandaferber03@users.noreply.github.com>
2022-07-09 21:53:26 -0400

    v

--- Table.cpp
+++ Table.cpp
@@ -205,6 +205,10 @@ namespace Solitaire
         }
         std::cout << std::endl;
         if(count > 0) {
+            std::cout << "    ";
+            for(int i = 1; i < count; i++) {
+                std::cout << "  ";
+            }
             std::cout << pile[pile_index]->to_ascii() << pile[pile_index]->get_suit();
         }
         std::cout << std::endl;
