94578e8738856cc518355936a1d55f1bd4306f10
amandaferber03 <98185007+amandaferber03@users.noreply.github.com>
2022-07-10 12:24:31 -0400

    v

--- Table.cpp
+++ Table.cpp
@@ -240,7 +240,7 @@ namespace Solitaire
             }
             if (has_cards[i]) {
                 std::cout << slots[suit_identifiers[i]][last_indices[k]]->to_ascii() << slots[suit_identifiers[i]][last_indices[i]]->get_suit();
-                k++
+                k++;
             }
             if(i > 0 && k > 0 && !has_cards[k-1]) {
                 std::cout << "       ";
