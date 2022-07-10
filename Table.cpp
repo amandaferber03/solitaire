13cbc2eb6ba5d83ace426c3df3fc505aa71eb67e
amandaferber03 <98185007+amandaferber03@users.noreply.github.com>
2022-07-09 23:40:00 -0400

    b

--- Table.cpp
+++ Table.cpp
@@ -255,7 +255,7 @@ namespace Solitaire
                     std::cout << slots[chars[j]][slots[chars[j]].size() - 1]->to_ascii() << get_string(slots[chars[j]][slots[chars[j]].size() - 1]->get_suit()).c_str();
                 }
                 if(i >= slots[chars[j]].size()) {
-                    std::cout << "      ";
+                    std::cout << "        ";
                     continue;
                 }
                 Terminal::color_bg(Terminal::WHITE);
